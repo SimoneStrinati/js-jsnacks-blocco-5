f96ade6fcec5a40c158e815e6aa916a141397195
simonestrinati <simone.strinati@outlook.it>
2025-11-26 10:29:51 +0100

    snack 1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,3 +2,14 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach(persona => {
+    console.log(persona)
+
+});
+
+/*********oppure function normale**********/
+
+names.forEach(function (persona) {
+    console.log(persona)
+})
