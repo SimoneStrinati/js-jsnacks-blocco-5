812c311d9ce5b8020986e56e0bcd94995920b8aa
simonestrinati <simone.strinati@outlook.it>
2025-11-26 11:09:36 +0100

    snack 6

--- ex-js-array-methods-main/snack-6/script.js
+++ ex-js-array-methods-main/snack-6/script.js
@@ -12,3 +12,16 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineCorte = zucchine.filter(zucchina => {
+  return zucchina.length < 15;
+})
+
+console.log(zucchineCorte);
+
+const altreZucchine = zucchine.filter(zucchina => {
+  return zucchina.length >= 15;
+})
+
+console.log(altreZucchine);
+
