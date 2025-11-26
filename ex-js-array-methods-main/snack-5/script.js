677ed56e05248d39e02254a0f078bc848b588da4
simonestrinati <simone.strinati@outlook.it>
2025-11-26 11:02:37 +0100

    snack 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,19 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const evenNumbers = nums.filter(function (numero){
+    //console.log(numero)
+    if (numero % 2 == 0){
+        return true;
+    }
+})
+
+console.log(evenNumbers);
+
+
+/*********oppure********/
+
+// const evenNumbers = nums.filter(numero => {
+//     return (numero % 2 == 0);
+// })
